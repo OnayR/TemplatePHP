e0c8e1d27cda8036e978fd8c2ab63c83535daa87
Rida Yousfi <128519567+OnayR@users.noreply.github.com>
2024-08-12 11:24:56 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,23 +7,9 @@ if (session_status() == PHP_SESSION_NONE) {
 // Parse the URI to route the request appropriately
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Dayl.ink</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<?php
-
 // A simple routing mechanism
 switch ($requestUri) {
     case '/':
       include __DIR__ . '/../pages/dashboard.php';
       break;
     }
-
-?>
-
-</html>
